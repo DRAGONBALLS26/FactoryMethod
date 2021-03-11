3812871e3eb641d41f4d3b4675c6cce783448551
DRAGONBALLS26 <71335022+DRAGONBALLS26@users.noreply.github.com>
2021-03-11 11:50:21 +0200

    Initial commit

---
+++ ConsoleApp6/CardFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp6
+{
+    abstract class CardFactory
+    {
+        public abstract CreditCard GetCreditCard();
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        public class ClientApplication
+        {
+            static void Main()
+            {
+                CardFactory factory = null;
+                Console.Write("Introduceti tipul de card: ");
+                string car = Console.ReadLine();
+
+                switch (car.ToLower())
+                {
+                    case "moneyback":
+                        factory = new MoneyBackFactory(50000, 0);
+                        break;
+                    case "titanium":
+                        factory = new TitaniumFactory(100000, 500);
+                        break;
+                    case "platinum":
+                        factory = new PlatinumFactory(500000, 1000);
+                        break;
+                    default:
+                        break;
+                }
+
+                CreditCard creditCard = factory.GetCreditCard();
+                Console.WriteLine("\nDetaliile cardului dumneavoastra : \n");
+                Console.WriteLine("Tipul Cardului: {0}\nLimita de Credit: {1}\nAlimentare anuala: {2}",
+                    creditCard.CardType, creditCard.CreditLimit, creditCard.AnnualCharge);
+                Console.ReadKey();
+            }
+        }
+    }
+}
